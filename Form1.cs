34ac4e8a450e06f00e4c2dcb772b6543cd12bced
Cheder21 <103432030+Cheder21@users.noreply.github.com>
2022-04-11 17:49:48 +0300

    Добавить дороги

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.Rows.Add(1, 2);
+            dataGridView1.Rows.Add(2, 3);
+            dataGridView1.Rows.Add(3, 4);
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Form2 form = new Form2();
+            for (int i = 0; i < dataGridView1.Rows.Count-1; i++)
+            {
+                form.comboBox1.Items.Add(dataGridView1.Rows[i].Cells[0].Value.ToString() + "," + dataGridView1.Rows[i].Cells[1].Value.ToString());
+                form.comboBox2.Items.Add(dataGridView1.Rows[i].Cells[0].Value.ToString() + "," + dataGridView1.Rows[i].Cells[1].Value.ToString());
+
+            }
+
+            form.Show();
+            
+        }
+    }
+}
